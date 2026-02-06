604d620f6c11fb2532fa24531a3bb5725db45ba6
jihad7518 <jihadmd7518@gmail.com>
2026-02-07 12:20:23 +1300

    .

---
+++ PSD/WEEK_07/Activity01/notification-system/version-1-without-factory/main.py
@@ -0,0 +1,18 @@
+from notification import EmailNotification, SMSNotification, PushNotification
+
+def main():
+    notification_type = input("Enter notification type (email/sms/push): ").lower()
+
+    if notification_type == "email":
+        notification = EmailNotification()
+    elif notification_type == "sms":
+        notification = SMSNotification()
+    elif notification_type == "push":
+        notification = PushNotification()
+    else:
+        raise ValueError("Invalid notification type")
+
+    notification.send("Hello! This is WITHOUT Factory Pattern.")
+
+if __name__ == "__main__":
+    main()
